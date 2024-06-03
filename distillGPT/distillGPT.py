7620fcd960b933eae36a2ade898d10a6c9485c11
HowardTsai6 <164084966+HowardTsai6@users.noreply.github.com>
2024-06-03 00:26:06 -0700

    split training and testing sets

--- distillGPT/distillGPT.py
+++ distillGPT/distillGPT.py
@@ -1,10 +1,18 @@
+import requests
 import torch
 from transformers import GPT2Tokenizer, GPT2LMHeadModel, AdamW, get_linear_schedule_with_warmup
-from torch.utils.data import Dataset, DataLoader
+from torch.utils.data import Dataset, DataLoader, random_split
 import re
-import requests
-from collections import defaultdict
 from tqdm import tqdm
+import nltk
+from nltk.corpus import brown
+
+nltk.download('brown')
+
+def load_text_from_url(url):
+    response = requests.get(url)
+    response.raise_for_status()
+    return response.text
 
 model_name = "distilgpt2"
 tokenizer = GPT2Tokenizer.from_pretrained(model_name)
@@ -12,58 +20,15 @@ model = GPT2LMHeadModel.from_pretrained(model_name)
 
 tokenizer.pad_token = tokenizer.eos_token
 
-
 def preprocess_text(text):
     text = text.lower()
     text = re.sub(r'[^a-z\s]', '', text)
     return text
 
-
-def generate_ngrams(text, N):
-    ngrams = defaultdict(int)
-    for i in range(len(text) - N + 1):
-        gram = text[i:i + N]
-        ngrams[gram] += 1
-    return ngrams
-
-
-def calculate_entropy(probabilities):
-    return -sum(p * torch.log(p) for p in probabilities if p > 0)
-
-
-def predict_next_char_and_entropy(context, model, tokenizer):
-    input_ids = tokenizer.encode(context, return_tensors='pt')
-    with torch.no_grad():
-        outputs = model(input_ids)
-        next_token_logits = outputs.logits[:, -1, :]
-        probabilities = torch.softmax(next_token_logits, dim=-1).squeeze()
-
-        valid_char_indices = [i for i in range(len(probabilities)) if re.match(r'^[a-z ]$', tokenizer.decode([i]))]
-        valid_probabilities = probabilities[valid_char_indices]
-        valid_tokens = [tokenizer.decode([i]) for i in valid_char_indices]
-
-        total_probability = valid_probabilities.sum()
-        normalized_probabilities = valid_probabilities / total_probability
-
-        entropy = calculate_entropy(normalized_probabilities)
-
-        predicted_token_index = normalized_probabilities.argmax()
-        next_char = valid_tokens[predicted_token_index]
-
-    return next_char, dict(zip(valid_tokens, normalized_probabilities)), entropy.item()
-
-
-def load_text_from_url(url):
-    response = requests.get(url)
-    response.raise_for_status()
-    return response.text
-
-
 def collate_fn(batch):
     input_ids = [item for item in batch]
     input_ids = torch.nn.utils.rnn.pad_sequence(input_ids, batch_first=True, padding_value=tokenizer.pad_token_id)
     return input_ids
-
 
 class TextDataset(Dataset):
     def __init__(self, text, tokenizer, block_size=128):
@@ -79,9 +44,14 @@ class TextDataset(Dataset):
     def __getitem__(self, item):
         return torch.tensor(self.examples[item], dtype=torch.long)
 
-
 def train(model, tokenizer, dataset, epochs=5, batch_size=32, lr=5e-5):
-    train_loader = DataLoader(dataset, batch_size=batch_size, shuffle=True, collate_fn=collate_fn)
+    train_size = int(0.7 * len(dataset))  # 70% for training sets
+    test_size = len(dataset) - train_size  # 30% for testing sets
+    train_dataset, test_dataset = random_split(dataset, [train_size, test_size])
+
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, collate_fn=collate_fn)
+    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False, collate_fn=collate_fn)
+
     optimizer = AdamW(model.parameters(), lr=lr)
     scheduler = get_linear_schedule_with_warmup(optimizer, num_warmup_steps=0,
                                                 num_training_steps=len(train_loader) * epochs)
@@ -100,20 +70,21 @@ def train(model, tokenizer, dataset, epochs=5, batch_size=32, lr=5e-5):
             optimizer.zero_grad()
             progress_bar.set_postfix(loss=loss.item())
 
+    model.eval()
+    total_loss = 0
+    with torch.no_grad():
+        for batch in test_loader:
+            inputs = batch.to(model.device)
+            outputs = model(inputs, labels=inputs)
+            total_loss += outputs.loss.item()
 
-url = "https://www.gutenberg.org/files/100/100-0.txt"   #Shakespear context
-text = load_text_from_url(url)
+    avg_loss = total_loss / len(test_loader)
+    print(f"Average test loss: {avg_loss}")
 
+
+# url = "https://www.gutenberg.org/files/100/100-0.txt"             #Shakespear Corpus
+# text = load_text_from_url(url)
+text = ' '.join(brown.words())                                      #Brown Corpus
 dataset = TextDataset(text, tokenizer)
 
 train(model, tokenizer, dataset, epochs=10, batch_size=32, lr=5e-5)
-
-processed_text = preprocess_text(text)
-N = 3  # trigram
-
-ngrams = [processed_text[i:i + N - 1] for i in range(len(processed_text) - N + 1)]
-
-print("Testing phase predictions:")
-for context in ngrams:
-    next_char, probabilities, entropy = predict_next_char_and_entropy(context, model, tokenizer)
-    print(f"Context: {context}, Next Char: {next_char}, Entropy: {entropy}")
