5cecbefd7e4dc3c070d79938199267d15287dab8
JayChao0331 <45892063+JayChao0331@users.noreply.github.com>
2024-06-06 19:58:27 -0700

    Update sample_code_brown.py

--- read_data/sample_code_brown.py
+++ read_data/sample_code_brown.py
@@ -2,6 +2,16 @@ from nltk.corpus import brown
 from read_brown import preprocess_word, preprocess_corpus, tokenizer, train_test_split
 
 
+def extract_unique_n_minus_1_grams(corpus, n):
+    n_minus_1_grams = set()
+    for sentence in corpus:
+        sentence = ''.join(sentence)
+        for i in range(len(sentence) - n + 2):
+            context = sentence[i:i + n - 1]
+            n_minus_1_grams.add(context)
+    return n_minus_1_grams
+
+
 
 if __name__ == '__main__':
     # Read Data
@@ -9,3 +19,4 @@ if __name__ == '__main__':
     corpus_processed = preprocess_corpus(corpus)
     corpus_tokenized = tokenizer(corpus_processed)
     train_set, test_set = train_test_split(corpus_tokenized, ratio=0.7)
+    test_n_minus_1_grams = extract_unique_n_minus_1_grams(test_set, n=N)
