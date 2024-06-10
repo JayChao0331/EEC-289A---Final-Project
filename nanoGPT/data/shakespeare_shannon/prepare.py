9c55e6bda938375b66445e6c3d103fbafe6e44d0
HC <51252433+bchou9@users.noreply.github.com>
2024-06-10 00:48:16 -0700

    Update prepare.py

--- nanoGPT/data/shakespeare_shannon/prepare.py
+++ nanoGPT/data/shakespeare_shannon/prepare.py
@@ -1,9 +1,3 @@
-"""
-Prepare the Shakespeare dataset for character-level language modeling.
-So instead of encoding with GPT-2 BPE tokens, we just map characters to ints.
-Will save train.bin, val.bin containing the ids, and meta.pkl containing the
-encoder and decoder and some other related info.
-"""
 import os
 import pickle
 import requests
@@ -13,26 +7,12 @@ sys.path.insert(1, 'C:/Users/lenovoi7/Documents/GitHub/EEC289AFinalProject/read_
 
 from sample_code_shakespeare import get_train_test_n_grams_data
 
-# # download the tiny shakespeare dataset
-# input_file_path = os.path.join(os.path.dirname(__file__), 'input.txt')
-# if not os.path.exists(input_file_path):
-#     data_url = 'https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt'
-#     with open(input_file_path, 'w') as f:
-#         f.write(requests.get(data_url).text)
-#
-# with open(input_file_path, 'r') as f:
-#     data = f.read()
-# print(f"length of dataset in characters: {len(data):,}")
 N = 3
 all_works_sentences, train_set, test_set, test_n_minus_1_grams = get_train_test_n_grams_data(N)
 
-# get all the unique characters that occur in this text
-#all_works_sentence = [''.join(x) for x in all_works_sentences]
+def flatten(list_in):
+    return [x for xs in list_in for x in xs]
 
-def flatten(xss):
-    return [x for xs in xss for x in xs]
-
-#print(all_works_senten)
 chars = 'abcdefghijklmnopqrstuvwxyz '
 vocab_size = len(chars)
 print("all the unique characters:", ''.join(set(chars)))
@@ -46,11 +26,6 @@ def encode(s):
     return [stoi[c] for c in s] # encoder: take a string, output a list of integers
 def decode(l):
     return ''.join([itos[i] for i in l]) # decoder: take a list of integers, output a string
-
-# create the train and test splits
-#n = len(data)
-# train_data = data[:int(n*0.9)]
-# test_data = data[int(n*0.9):]
 
 # encode both to integers
 train_set_1 = flatten(train_set)
